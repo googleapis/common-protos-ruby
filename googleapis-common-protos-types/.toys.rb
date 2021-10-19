60e4ff030b0d1cd8d73eba3ea6ef39f5a33d4cd3
Daniel Azuma <dazuma@google.com>
2021-10-19 07:18:30 -0700

    fix: Remove cloud-specific extended_operations proto

--- googleapis-common-protos-types/.toys.rb
+++ googleapis-common-protos-types/.toys.rb
@@ -20,7 +20,6 @@ PROTO_GLOBS = [
   "../googleapis/google/longrunning/*.proto",
   "../googleapis/google/rpc/*.proto",
   "../googleapis/google/type/*.proto",
-  "../googleapis/google/cloud/extended_operations.proto",
 ]
 
 tool "compile" do
