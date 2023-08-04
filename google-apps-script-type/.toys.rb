8f46dc81813f8e8faf606ae04cb8b6c9ec5429b9
Daniel Azuma <dazuma@google.com>
2023-08-04 13:10:21 -0700

    chore: Consolidate compile scripts and add proto comments (#161)

--- google-apps-script-type/.toys.rb
+++ google-apps-script-type/.toys.rb
@@ -14,52 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PROTO_GLOBS = [
-  "../googleapis/google/apps/script/type/**/*.proto",
+load "#{context_directory}/../shared/toys-common.rb"
+
+proto_globs = [
+  "google/apps/script/type/**/*.proto",
 ]
 
-tool "compile" do
-  flag :clean, "--[no-]clean", default: true
-
-  include :fileutils
-  include :exec, e: true
-  include :gems
-
-  def run
-    cd context_directory
-    gem "grpc-tools", "~> 1.50"
-    Dir.glob("lib/**/*_pb.rb") { |path| rm path } if clean
-    cmd = [
-      "grpc_tools_ruby_protoc",
-      "--ruby_out=lib",
-      "-I", "../googleapis"
-    ] + PROTO_GLOBS.flat_map { |glob| Dir.glob glob }
-    exec cmd
-  end
-end
-
-tool "test" do
-  include :fileutils
-  include :exec, e: true
-  include :bundler
-
-  def run
-    cd context_directory
-    exec_ruby [], in: :controller do |controller|
-      controller.in.puts "puts 'Loading proto files'"
-      controller.in.puts "$LOAD_PATH.unshift File.expand_path 'lib'"
-      cd "lib" do
-        Dir.glob("google/**/*_pb.rb") do |path|
-          controller.in.puts "puts #{path.inspect}"
-          controller.in.puts "require #{path.inspect}"
-        end
-      end
-    end
-  end
-end
-
-expand :clean, paths: :gitignore
-
-expand :gem_build
-
-expand :gem_build, name: "release", push_gem: true
+expand "common-proto-tools", proto_globs: proto_globs,
+                             include_proto_comments: true
