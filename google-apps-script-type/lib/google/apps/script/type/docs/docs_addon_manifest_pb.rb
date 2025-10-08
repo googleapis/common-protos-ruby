781bb7ca1f2fda20ca733cfd0e012196db51a97e
André Andreassa <andreassa@google.com>
2025-10-07 17:41:55 -0700

    chore: Regenerate gems

--- google-apps-script-type/lib/google/apps/script/type/docs/docs_addon_manifest_pb.rb
+++ google-apps-script-type/lib/google/apps/script/type/docs/docs_addon_manifest_pb.rb
@@ -10,30 +10,8 @@ require 'google/apps/script/type/extension_point_pb'
 
 descriptor_data = "\n6google/apps/script/type/docs/docs_addon_manifest.proto\x12\x1cgoogle.apps.script.type.docs\x1a\x1fgoogle/api/field_behavior.proto\x1a-google/apps/script/type/extension_point.proto\"\xb7\x01\n\x11\x44ocsAddOnManifest\x12I\n\x10homepage_trigger\x18\x01 \x01(\x0b\x32/.google.apps.script.type.HomepageExtensionPoint\x12W\n\x1don_file_scope_granted_trigger\x18\x02 \x01(\x0b\x32\x30.google.apps.script.type.docs.DocsExtensionPoint\"/\n\x12\x44ocsExtensionPoint\x12\x19\n\x0crun_function\x18\x01 \x01(\tB\x03\xe0\x41\x02\x42\xda\x01\n com.google.apps.script.type.docsB\x16\x44ocsAddOnManifestProtoP\x01Z;google.golang.org/genproto/googleapis/apps/script/type/docs\xaa\x02\x1cGoogle.Apps.Script.Type.Docs\xca\x02\x1cGoogle\\Apps\\Script\\Type\\Docs\xea\x02 Google::Apps::Script::Type::Docsb\x06proto3"
 
-pool = Google::Protobuf::DescriptorPool.generated_pool
-
-begin
-  pool.add_serialized_file(descriptor_data)
-rescue TypeError
-  # Compatibility code: will be removed in the next major version.
-  require 'google/protobuf/descriptor_pb'
-  parsed = Google::Protobuf::FileDescriptorProto.decode(descriptor_data)
-  parsed.clear_dependency
-  serialized = parsed.class.encode(parsed)
-  file = pool.add_serialized_file(serialized)
-  warn "Warning: Protobuf detected an import path issue while loading generated file #{__FILE__}"
-  imports = [
-    ["google.apps.script.type.HomepageExtensionPoint", "google/apps/script/type/extension_point.proto"],
-  ]
-  imports.each do |type_name, expected_filename|
-    import_file = pool.lookup(type_name).file_descriptor
-    if import_file.name != expected_filename
-      warn "- #{file.name} imports #{expected_filename}, but that import was loaded as #{import_file.name}"
-    end
-  end
-  warn "Each proto file must use a consistent fully-qualified name."
-  warn "This will become an error in the next major version."
-end
+pool = ::Google::Protobuf::DescriptorPool.generated_pool
+pool.add_serialized_file(descriptor_data)
 
 module Google
   module Apps
